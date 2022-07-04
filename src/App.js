9617d98704496e1eca739c32cf157e3f2f118c8f
raamprasaadh <ramprasadhsurya@gmail.com>
2022-07-04 18:12:28 +0530

    adds images & nav skeleton

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1 className='text-blue-500'>helloworld</h1>
+      <div className="flex items-center justify-between">
+
+      </div>
+    <nav className="relative container">
+
+    </nav>
     </div>
   );
 }
